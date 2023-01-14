273cb92513f95eae55b8d06663a23ca463f39099
Sergey Gromakov <sisolin@icloud.com>
2023-01-14 21:54:20 +0300

    Example 004 Random

--- Examples/Example_004_Div/Program.cs
+++ Examples/Example_004_Div/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿double numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
